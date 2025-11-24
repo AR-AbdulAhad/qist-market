bd30d1788938b29af6ed42581f619cb69ef46482
malidaddydev <abdulahad010274@gmail.com>
2025-11-24 21:38:19 +0500

    updated

---
+++ app/home_sitemap.xml/route.js
@@ -0,0 +1,12 @@
+export async function GET(request) {
+  const { pathname } = new URL(request.url);
+
+  const backendUrl = `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/home_sitemap.xml`;
+
+  const res = await fetch(backendUrl, { next: { revalidate: 3600 } });
+  const xml = await res.text();
+
+  return new Response(xml, {
+    headers: { 'Content-Type': 'application/xml' }
+  });
+}

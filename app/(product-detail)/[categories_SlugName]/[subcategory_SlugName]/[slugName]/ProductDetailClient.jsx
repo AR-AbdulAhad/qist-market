70055ed202158f609da558c22fce6249b458264a
malidaddydev <abdulahad010274@gmail.com>
2025-12-01 15:21:52 +0500

    updated

--- app/(product-detail)/[categories_SlugName]/[subcategory_SlugName]/[slugName]/ProductDetailClient.jsx
+++ app/(product-detail)/[categories_SlugName]/[subcategory_SlugName]/[slugName]/ProductDetailClient.jsx
@@ -2,9 +2,9 @@
 
 import Footer1 from "@/components/footers/Footer1";
 import Header4 from "@/components/headers/Header4";
-import Description from "@/components/product-detail/Description";
+// import Description from "@/components/product-detail/Description";
 import Details1 from "@/components/product-detail/Details1";
-import Relatedproducts from "@/components/product-detail/Relatedproducts";
+// import Relatedproducts from "@/components/product-detail/Relatedproducts";
 
 export default function ProductDetailClient({ product }) {
   return (
